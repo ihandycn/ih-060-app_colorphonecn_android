cd9933ad0e3baf242c5b16ddcc32bfede8fad826
Duanxing Sun <duanxing.sun@ihandysoft.com>
2017-07-07 22:13:50 +0800

    Preview activity auto hide actions after on touch in  4 sec.

--- app/src/main/java/com/honeycomb/colorphone/ThemePreviewActivity.java
+++ app/src/main/java/com/honeycomb/colorphone/ThemePreviewActivity.java
@@ -6,7 +6,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.OvershootInterpolator;
 import android.widget.Button;
@@ -32,18 +35,54 @@ public class ThemePreviewActivity extends HSAppCompatActivity {
 
     public static final String NOTIFY_THEME_SELECT = "notify_theme_select";
     public static final String NOTIFY_THEME_SELECT_KEY = "notify_theme_select_key";
+
+    private static final int MSG_HIDE = 1;
+    private static final int MSG_SHOW = 2;
+    private static final long AUTO_HIDE_TIME = 4000;
+    private static final long ANIMATION_DURATION = 400;
+
     private ThemePreviewWindow previewWindow;
     private InCallActionView callActionView;
 
     private ProgressViewHolder mProgressViewHolder;
     private Button mApplyButton;
+    private View mNavBack;
+
     private ImageView previewImage;
     private Theme mTheme;
     private Type mThemeType;
     private View dimCover;
     private int curTaskId;
+    /**
+     * If button is playing animation
+     */
+    private boolean inTransition;
+    private boolean themeReady;
 
+    private long animationDelay = 500;
+    private float bottomBtnTransY;
 
+    private Handler mHandler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_HIDE:
+                    showNavView(false);
+                    fadeOutActionView();
+                    return true;
+
+                case MSG_SHOW:
+                    showNavView(true);
+                    fadeInActionView();
+                    return true;
+
+                default:
+                    return false;
+
+            }
+        }
+    });
+    private OvershootInterpolator mInter;
 
     public static void start(Context context, Theme theme) {
         Intent starter = new Intent(context, ThemePreviewActivity.class);
@@ -71,6 +110,12 @@ public class ThemePreviewActivity extends HSAppCompatActivity {
     };
 
     @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        return super.onTouchEvent(event);
+    }
+
+    @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mTheme = (Theme) getIntent().getSerializableExtra("theme");
@@ -86,6 +131,20 @@ public class ThemePreviewActivity extends HSAppCompatActivity {
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
 
         previewWindow = (ThemePreviewWindow) findViewById(R.id.card_flash_preview_window);
+        previewWindow.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (themeReady && !inTransition) {
+                    boolean isInHide = mApplyButton.getTranslationY() == bottomBtnTransY;
+                    if (isInHide) {
+                        mHandler.sendEmptyMessage(MSG_SHOW);
+                    }
+                    scheduleNextHide();
+
+                }
+                return true;
+            }
+        });
         callActionView = (InCallActionView) findViewById(R.id.card_in_call_action_view);
         callActionView.setTheme(mThemeType);
         callActionView.setAutoRun(false);
@@ -94,7 +153,8 @@ public class ThemePreviewActivity extends HSAppCompatActivity {
         previewImage = (ImageView) findViewById(R.id.preview_bg_img);
         dimCover = findViewById(R.id.dim_cover);
 
-        findViewById(R.id.nav_back).setOnClickListener(new View.OnClickListener() {
+        mNavBack = findViewById(R.id.nav_back);
+        mNavBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onBackPressed();
@@ -104,6 +164,9 @@ public class ThemePreviewActivity extends HSAppCompatActivity {
         mApplyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (inTransition) {
+                    return;
+                }
                 CPSettings.putInt(CPSettings.PREFS_SCREEN_FLASH_SELECTOR_INDEX, mTheme.getThemeId());
                 // notify
                 HSBundle bundle = new HSBundle();
@@ -114,6 +177,9 @@ public class ThemePreviewActivity extends HSAppCompatActivity {
                 Toast.makeText(ThemePreviewActivity.this, R.string.apply_success, Toast.LENGTH_SHORT).show();
             }
         });
+        bottomBtnTransY = mApplyButton.getTranslationY();
+
+        mInter = new OvershootInterpolator(1.5f);
 
     }
 
@@ -121,7 +187,7 @@ public class ThemePreviewActivity extends HSAppCompatActivity {
         mProgressViewHolder.fadeOut();
         dimCover.animate().alpha(0).setDuration(200);
         mApplyButton.setVisibility(View.VISIBLE);
-        previewWindow.playAnimation(mThemeType);
+        animationDelay = 0;
         setButtonState(false);
 
     }
@@ -133,29 +199,62 @@ public class ThemePreviewActivity extends HSAppCompatActivity {
 
     private void onThemeReady() {
         boolean curTheme = CPSettings.getInt(CPSettings.PREFS_SCREEN_FLASH_SELECTOR_INDEX, -1) == mTheme.getThemeId();
-        previewWindow.playAnimation(mThemeType);
-        callActionView.doAnimation();
+
         setButtonState(curTheme);
     }
 
     private void setButtonState(final boolean curTheme) {
+        themeReady = true;
+
         dimCover.setVisibility(View.INVISIBLE);
         mProgressViewHolder.hide();
+        previewWindow.playAnimation(mThemeType);
+        callActionView.doAnimation();
 
         if (curTheme) {
             mApplyButton.setText(getString(R.string.theme_current));
         } else {
             mApplyButton.setText(getString(R.string.theme_apply));
         }
-        mApplyButton.animate().translationY(0).setDuration(400).setInterpolator(new OvershootInterpolator()).setListener(new AnimatorListenerAdapter() {
+        mApplyButton.setEnabled(!curTheme);
+
+        fadeInActionView();
+    }
+
+    private void showNavView(boolean show) {
+        float offsetX = Utils.pxFromDp(60);
+        mNavBack.animate().translationX(show ? 0 : -offsetX).setDuration(ANIMATION_DURATION).setInterpolator(mInter).start();
+    }
+
+    private void fadeInActionView() {
+        inTransition = true;
+        mApplyButton.animate().translationY(0).setDuration(ANIMATION_DURATION).setInterpolator(mInter).setListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(Animator animation) {
-                mApplyButton.setVisibility(View.VISIBLE);
                 mApplyButton.setTranslationY(0);
-                mApplyButton.setEnabled(curTheme ? false : true);
+                inTransition = false;
+                animationDelay = 0;
+                scheduleNextHide();
+
+            }
+        }).setStartDelay(animationDelay).start();
+    }
+
+    private void fadeOutActionView() {
+        inTransition = true;
+        mApplyButton.animate().translationY(bottomBtnTransY).setDuration(ANIMATION_DURATION).setInterpolator(mInter).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mApplyButton.setTranslationY(bottomBtnTransY);
+                inTransition = false;
 
             }
         }).start();
+    }
+
+    private void scheduleNextHide() {
+        mHandler.removeMessages(MSG_HIDE);
+        mHandler.sendEmptyMessageDelayed(MSG_HIDE, AUTO_HIDE_TIME);
     }
 
     @Override
@@ -206,6 +305,10 @@ public class ThemePreviewActivity extends HSAppCompatActivity {
         callActionView.stopAnimations();
         previewWindow.stopAnimations();
         FileDownloadMultiListener.getDefault().removeStateListener(curTaskId);
+
+        mNavBack.animate().cancel();
+        mApplyButton.animate().cancel();
+        mHandler.removeCallbacksAndMessages(null);
     }
 
     private class ProgressViewHolder {
