5d8de0cbf6a5a6dbc414a2a93a4cda89bebec3b8
dongguo <dongguo@ihandysoft.com>
2017-12-01 14:46:27 +0800

    add base files

---
+++ app/src/main/java/com/honeycomb/colorphone/activity/ShareAlertActivity.java
@@ -0,0 +1,16 @@
+package com.honeycomb.colorphone.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by jelly on 2017/12/1.
+ */
+
+public class ShareAlertActivity extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
