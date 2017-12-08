0b0ed59cd3d2af447dc5ddbf0433eb10c0a9039e
Duanxing Sun <duanxing.sun@ihandysoft.com>
2017-12-08 13:39:35 +0800

    Test activity can edit phone number to test incoming-call-window.

--- app/src/main/java/com/honeycomb/colorphone/activity/TestActivity.java
+++ app/src/main/java/com/honeycomb/colorphone/activity/TestActivity.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 
 import com.acb.call.service.InCallWindow;
 import com.honeycomb.colorphone.R;
@@ -16,15 +18,17 @@ import com.honeycomb.colorphone.R;
 public class TestActivity extends AppCompatActivity {
 
     Handler mHandler = new Handler();
-
+    EditText editText;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        editText = findViewById(R.id.test_number);
     }
 
     public void startCallRingingWindow(View view) {
-        InCallWindow.show(this, "8888888");
+        String number = editText.getText().toString().trim();
+        InCallWindow.show(this, TextUtils.isEmpty(number) ? "8888888" : number);
         mHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
