0c9b13667989cf004942036090b2e077ec7d3bff
zhewang <zhe.wang@ihandysoft.com>
2018-01-10 14:45:41 +0800

    Revert "Merge branch 'memory_boost' into develop"
    This reverts commit 5f274ea7c17d6b1be3cb1630878bb0a27a3b2d57, reversing
    changes made to cbf442a19ad66b7f68f9bf7a88c610e0839a9fb3.

--- app/src/main/java/com/honeycomb/colorphone/AdPlacements.java
+++ app/src/main/java/com/honeycomb/colorphone/AdPlacements.java
@@ -7,8 +7,7 @@ package com.honeycomb.colorphone;
 public class AdPlacements {
     public static final String AD_CALL_OFF = "ColorPhone_A(NativeAds)CallOff";
     public static final String AD_MSG_NEW = "ColorPhone_A(NativeAds)Texture";
-    public static final String AD_CHARGING_SCREEN = "ColorPhone_A(ExpressAds)Cable";
+    public static final String AD_CHAEGING_SCREEN = "ColorPhone_A(ExpressAds)Cable";
     public static final String AD_LOCKER = "ColorPhone_A(ExpressAds)Fuse";
     public static final String AD_CHARGING_REPORT = "ColorPhone_A(NativeAds)ChargingReport";
-    public static final String AD_RESULT_PAGE = "ColorPhone_A(NativeAds)ChargingReport";
 }
