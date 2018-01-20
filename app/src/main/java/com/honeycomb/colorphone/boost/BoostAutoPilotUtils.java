e9e60f4e7f457cc8ba4fa7fa178d8f8231a1f947
zhewang <zhe.wang@ihandysoft.com>
2018-01-20 10:49:53 +0800

    add autopilot info log

--- app/src/main/java/com/honeycomb/colorphone/boost/BoostAutoPilotUtils.java
+++ app/src/main/java/com/honeycomb/colorphone/boost/BoostAutoPilotUtils.java
@@ -4,7 +4,7 @@ import android.text.format.DateUtils;
 
 import com.acb.autopilot.AutopilotConfig;
 import com.acb.autopilot.AutopilotEvent;
-import com.honeycomb.colorphone.BuildConfig;
+import com.honeycomb.colorphone.notification.NotificationCondition;
 import com.ihs.commons.utils.HSLog;
 
 /**
@@ -16,42 +16,42 @@ public class BoostAutoPilotUtils {
 
     public static boolean isBoostPushEnable() {
         boolean enable = AutopilotConfig.getBooleanToTestNow(BOOST_TEST_TOPIC_ID, "boost_push_enable", false);
-        if (BuildConfig.DEBUG) {
-            HSLog.i("Boost", "Debug isBoostPushEnable == " + enable);
-//            return true;
+        HSLog.i(NotificationCondition.TAG, "Debug isBoostPushEnable == " + enable);
+        if (NotificationCondition.DEBUG_BOOST_PLUS_NOTIFICATION) {
+            return true;
         }
         return enable;
     }
 
     public static double getBoostPushInterval() {
         double interval = AutopilotConfig.getDoubleToTestNow(BOOST_TEST_TOPIC_ID, "boost_push_interval", 2);
-        if (BuildConfig.DEBUG) {
-            HSLog.i("Boost", "Debug getBoostPushInterval == " + interval);
-//            return 0;
+        HSLog.i(NotificationCondition.TAG, "Debug getBoostPushInterval == " + interval);
+        if (NotificationCondition.DEBUG_BOOST_PLUS_NOTIFICATION) {
+            return 0;
         }
         return DateUtils.HOUR_IN_MILLIS * interval;
     }
 
     public static int getBoostPushMaxCount() {
         double max = AutopilotConfig.getDoubleToTestNow(BOOST_TEST_TOPIC_ID, "boost_push_maxcount", 4);
-        if (BuildConfig.DEBUG) {
-            HSLog.i("Boost", "Debug getBoostPushMaxCount == " + max);
-//            return 100;
+        HSLog.i(NotificationCondition.TAG, "Debug getBoostPushMaxCount == " + max);
+        if (NotificationCondition.DEBUG_BOOST_PLUS_NOTIFICATION) {
+            return 100;
         }
         return (int) max;
     }
 
     public static void logBoostPushShow() {
-        HSLog.i("Boost", "boost_push_show");
+        HSLog.i(NotificationCondition.TAG, "boost_push_show");
         AutopilotEvent.logTopicEvent(BOOST_TEST_TOPIC_ID, "boost_push_show");
     }
 
     public static void logBoostPushClicked() {
-        HSLog.i("Boost", "boost_push_clicked");
+        HSLog.i(NotificationCondition.TAG, "boost_push_clicked");
         AutopilotEvent.logTopicEvent(BOOST_TEST_TOPIC_ID, "boost_push_clicked");
     }
     public static void logBoostPushAdShow() {
-        HSLog.i("Boost", "boost_ad_show");
+        HSLog.i(NotificationCondition.TAG, "boost_ad_show");
         AutopilotEvent.logTopicEvent(BOOST_TEST_TOPIC_ID, "boost_ad_show");
     }
 
