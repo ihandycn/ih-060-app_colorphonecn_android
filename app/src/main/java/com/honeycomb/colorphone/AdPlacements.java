8618f3cdc685fcd7ad93ba75cec8244733ca5e24
dongguo <dongguo@ihandysoft.com>
2018-03-20 17:44:47 +0800

    update goldenEye

--- app/src/main/java/com/honeycomb/colorphone/AdPlacements.java
+++ app/src/main/java/com/honeycomb/colorphone/AdPlacements.java
@@ -11,7 +11,7 @@ public class AdPlacements {
     public static final String AD_MSG_NEW = "ColorPhone_A(ExpressAds)Texture";
     public static final String AD_CHARGING_SCREEN = "ColorPhone_A(ExpressAds)Cable";
     public static final String AD_LOCKER = "ColorPhone_A(ExpressAds)Fuse";
-    public static final String AD_CHARGING_REPORT = "ColorPhone_A(NativeAds)ChargingReport";
+    public static final String AD_CHARGING_REPORT = "ChargingReportPlus";
     public static final String AD_RESULT_PAGE = "ColorPhone_A(NativeAds)Boost";
     public static final String AD_RESULT_PAGE_INTERSTITIAL = "ColorPhone_A(InterstitialAds)Weel";
     public static final String SMART_ASSISTANT_PLACEMENT_NAME = "RecentAppsPlus";
