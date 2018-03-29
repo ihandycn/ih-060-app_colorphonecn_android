3e8f510dba358049ea6f7591e1ed45a94f0c9074
zhewang <zhe.wang@ihandysoft.com>
2018-03-29 15:16:51 +0800

    fix colorflash ui

---
+++ app/src/colorflash/java/com/honeycomb/colorphone/AdPlacements.java
@@ -0,0 +1,19 @@
+package com.honeycomb.colorphone;
+
+/**
+ * Created by sundxing on 17/9/8.
+ */
+
+public class AdPlacements {
+//    public static final String AD_CALL_OFF = "ColorPhone_A(NativeAds)CallOff";
+//    public static final String AD_MSG_NEW = "ColorPhone_A(NativeAds)Texture";
+    public static final String AD_CALL_OFF = "Lumen";
+    public static final String AD_MSG_NEW = "Texture";
+    public static final String AD_CHARGING_SCREEN = "ColorPhone_A(ExpressAds)Cable";
+    public static final String AD_LOCKER = "ColorPhone_A(ExpressAds)Fuse";
+    public static final String AD_CHARGING_REPORT = "ChargingReport";
+    public static final String AD_RESULT_PAGE = "Boost";
+    public static final String AD_RESULT_PAGE_INTERSTITIAL = "Weel";
+    public static final String SMART_ASSISTANT_PLACEMENT_NAME = "RecentApps";
+
+}
---
+++ app/src/colorphone/java/com/honeycomb/colorphone/AdPlacements.java
@@ -0,0 +1,18 @@
+package com.honeycomb.colorphone;
+
+/**
+ * Created by sundxing on 17/9/8.
+ */
+
+public class AdPlacements {
+//    public static final String AD_CALL_OFF = "ColorPhone_A(NativeAds)CallOff";
+//    public static final String AD_MSG_NEW = "ColorPhone_A(NativeAds)Texture";
+    public static final String AD_CALL_OFF = "ColorPhone_A(ExpressAds)CallOff";
+    public static final String AD_MSG_NEW = "ColorPhone_A(ExpressAds)Texture";
+    public static final String AD_CHARGING_SCREEN = "ColorPhone_A(ExpressAds)Cable";
+    public static final String AD_LOCKER = "ColorPhone_A(ExpressAds)Fuse";
+    public static final String AD_CHARGING_REPORT = "ChargingReportPlus";
+    public static final String AD_RESULT_PAGE = "ColorPhone_A(NativeAds)Boost";
+    public static final String AD_RESULT_PAGE_INTERSTITIAL = "ColorPhone_A(InterstitialAds)Weel";
+    public static final String SMART_ASSISTANT_PLACEMENT_NAME = "RecentAppsPlus";
+}
