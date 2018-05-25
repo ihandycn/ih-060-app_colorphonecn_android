1bbb8fec0649f5a5f0c3927cc223be1748a20d8e
Xiao Zheng <xiao.zheng@ihandysoft.com>
2018-05-25 20:11:42 +0800

    修改policy地址

--- app/src/main/java/com/honeycomb/colorphone/Constants.java
+++ app/src/main/java/com/honeycomb/colorphone/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
     public static final int DEFAULT_THEME_ID = 14; // Shining
     public static final String FEED_BACK_EMAIL = "feedback.colorphone@gmail.com";
-    public static final String URL_PRIVACY = "http://colorphone.weebly.com/";
+    public static final String URL_PRIVACY = "https://colorphoneflash.weebly.com/";
     public static final String URL_TERM_SERVICES = "http://colorphone.weebly.com/terms-of-service.html";
     public static final String NOTIFICATION_PREFS = "notification.prefs";
     public static final String DESKTOP_PREFS = "desktop.prefs";
