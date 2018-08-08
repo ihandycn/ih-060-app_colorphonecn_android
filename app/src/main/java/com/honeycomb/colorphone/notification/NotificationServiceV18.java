0852004c3984e5c12e5f5d83944b410e8db33f2a
Duanxing Sun <duanxing.sun@ihandysoft.com>
2018-08-08 15:19:26 +0800

    Ensure notification service active. ( Try fix accept call fail)

--- app/src/main/java/com/honeycomb/colorphone/notification/NotificationServiceV18.java
+++ app/src/main/java/com/honeycomb/colorphone/notification/NotificationServiceV18.java
@@ -2,13 +2,21 @@ package com.honeycomb.colorphone.notification;
 
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
+import android.app.ActivityManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Build;
+import android.os.Process;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
+import android.util.Log;
 
 import com.acb.call.CallIntentManager;
 import com.ihs.commons.utils.HSLog;
 import com.messagecenter.customize.MessageCenterManager;
+
+import java.util.List;
 
 @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
 public class NotificationServiceV18 extends NotificationListenerService {
@@ -22,6 +30,11 @@ public class NotificationServiceV18 extends NotificationListenerService {
     @Override
     public void onCreate() {
         super.onCreate();
+        try {
+            ensureCollectorRunning();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         HSLog.i(TAG, "NotificationListenerService created");
     }
 
@@ -64,5 +77,47 @@ public class NotificationServiceV18 extends NotificationListenerService {
             notifications = new StatusBarNotification[0];
         }
         return notifications;
+    }
+
+    /**
+     * Try fix service not active.
+     *
+     *  https://gist.github.com/xinghui/b2ddd8cffe55c4b62f5d8846d5545bf9
+     *  https://www.zhihu.com/question/33540416
+     */
+    private void ensureCollectorRunning() {
+        ComponentName collectorComponent = new ComponentName(this, /*NotificationListenerService Inheritance*/ NotificationServiceV18.class);
+        Log.v(TAG, "ensureCollectorRunning collectorComponent: " + collectorComponent);
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        boolean collectorRunning = false;
+        List<ActivityManager.RunningServiceInfo> runningServices = manager.getRunningServices(Integer.MAX_VALUE);
+        if (runningServices == null ) {
+            Log.w(TAG, "ensureCollectorRunning() runningServices is NULL");
+            return;
+        }
+        for (ActivityManager.RunningServiceInfo service : runningServices) {
+            if (service.service.equals(collectorComponent)) {
+                Log.w(TAG, "ensureCollectorRunning service - pid: " + service.pid + ", currentPID: " + Process.myPid() + ", clientPackage: " + service.clientPackage + ", clientCount: " + service.clientCount
+                        + ", clientLabel: " + ((service.clientLabel == 0) ? "0" : "(" + getResources().getString(service.clientLabel) + ")"));
+                if (service.pid == Process.myPid() /*&& service.clientCount > 0 && !TextUtils.isEmpty(service.clientPackage)*/) {
+                    collectorRunning = true;
+                }
+            }
+        }
+        if (collectorRunning) {
+            Log.d(TAG, "ensureCollectorRunning: collector is running");
+            return;
+        }
+        Log.d(TAG, "ensureCollectorRunning: collector not running, reviving...");
+        toggleNotificationListenerService();
+    }
+
+    private void toggleNotificationListenerService() {
+        Log.d(TAG, "toggleNotificationListenerService() called");
+        ComponentName thisComponent = new ComponentName(this, /*getClass()*/ NotificationServiceV18.class);
+        PackageManager pm = getPackageManager();
+        pm.setComponentEnabledSetting(thisComponent, PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+        pm.setComponentEnabledSetting(thisComponent, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
+
     }
 }
