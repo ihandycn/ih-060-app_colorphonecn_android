4cce6fddc9afe8b9841299a90999268642999104
Duanxing Sun <duanxing.sun@ihandysoft.com>
2018-09-11 20:32:54 +0800

    Event to flurry & fabric.

--- app/src/main/java/com/honeycomb/colorphone/dialer/AP.java
+++ app/src/main/java/com/honeycomb/colorphone/dialer/AP.java
@@ -1,5 +1,7 @@
 package com.honeycomb.colorphone.dialer;
 
+import com.honeycomb.colorphone.util.LauncherAnalytics;
+
 import net.appcloudbox.autopilot.AutopilotConfig;
 import net.appcloudbox.autopilot.AutopilotEvent;
 
@@ -16,20 +18,37 @@ public class AP {
 
     public static void guideShow() {
         AutopilotEvent.logTopicEvent("topic-1536215679114-660", "set_default_guide_show");
+        LauncherAnalytics.logEvent(upperFirstCh("set_default_guide_show"));
     }
 
     public static void guideConfirmed() {
         AutopilotEvent.logTopicEvent("topic-1536215679114-660", "set_default_guide_set_clicked");
+        LauncherAnalytics.logEvent(upperFirstCh("set_default_guide_set_clicked"));
     }
 
     public static void successSetAsDefault() {
         AutopilotEvent.logTopicEvent("topic-1536215679114-660", "set_default_success");
+        LauncherAnalytics.logEvent(upperFirstCh("set_default_success"));
     }
 
     public static void dilerShow() {
         AutopilotEvent.logTopicEvent("topic-1536215679114-660", "dailer_page_show");
+        LauncherAnalytics.logEvent(upperFirstCh("dailer_page_show"));
     }
 
-
+    private static String upperFirstCh(String event) {
+        StringBuilder sb = new StringBuilder();
+        char aheadCh = 0;
+        char spitCh = '_';
+        for (int i = 0; i < event.length(); i++) {
+            if (aheadCh == 0 || aheadCh == spitCh) {
+                sb.append(Character.toUpperCase(event.charAt(i)));
+            } else {
+                sb.append(event.charAt(i));
+            }
+            aheadCh = event.charAt(i);
+        }
+        return sb.toString();
+    }
 
 }
