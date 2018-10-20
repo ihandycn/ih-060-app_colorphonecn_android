e086a837b9431ca1f478aa665cf19d5b66810f87
Duanxing Sun <duanxing.sun@ihandysoft.com>
2018-10-20 15:49:41 +0800

    Add cash utils.

--- app/src/main/java/com/honeycomb/colorphone/cashcenter/Utils.java
+++ app/src/main/java/com/honeycomb/colorphone/cashcenter/Utils.java
@@ -1,4 +1,15 @@
 package com.honeycomb.colorphone.cashcenter;
 
+import com.superapps.util.Preferences;
+
 public class Utils {
+
+    public static boolean hasUserEnterCrashCenter() {
+        return Preferences.get("cash_center").getBoolean("user_visit", false);
+    }
+    public static void userEnterCashCenter() {
+        Preferences.get("cash_center").putBoolean("user_visit", true);
+    }
+
+
 }
