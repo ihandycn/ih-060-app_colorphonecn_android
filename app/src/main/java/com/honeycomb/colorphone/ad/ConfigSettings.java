efb130b6e5407e4826ca23e5beea7b8de4f3baf2
duanxing.sun <duanxing.sun@ihandysoft.com>
2018-12-21 18:22:29 +0800

    Add config "FullScreen" - "EnableVersionList"

--- app/src/main/java/com/honeycomb/colorphone/ad/ConfigSettings.java
+++ app/src/main/java/com/honeycomb/colorphone/ad/ConfigSettings.java
@@ -1,11 +1,17 @@
 package com.honeycomb.colorphone.ad;
 
 import com.honeycomb.colorphone.Ap;
+import com.ihs.app.framework.HSApplication;
 import com.ihs.commons.config.HSConfig;
 import com.ihs.commons.utils.HSLog;
 
+import java.util.List;
+
 public class ConfigSettings {
     public static boolean showAdOnApplyTheme() {
+        if (!enabledThisVersion()) {
+            return false;
+        }
         boolean configEnable = HSConfig.optBoolean(false,"Application", "FullScreen", "ThemeApply");
         boolean autopilotEnable = Ap.DetailAd.enableAdOnApply();
         HSLog.d("ConfigSettings", "AdOnApplyTheme:" + configEnable + "," + autopilotEnable);
@@ -13,9 +19,21 @@ public class ConfigSettings {
     }
 
     public static boolean showAdOnDetailView() {
+        if (!enabledThisVersion()) {
+            return false;
+        }
         boolean configEnable = HSConfig.optBoolean(false,"Application", "FullScreen", "DetailView");
         boolean autopilotEnable = Ap.DetailAd.enableAdOnDetailView();
         HSLog.d("ConfigSettings", "AdOnDetailView:" + configEnable + "," + autopilotEnable);
         return configEnable && autopilotEnable;
+    }
+
+    private static boolean enabledThisVersion() {
+        List<Integer> enableList = (List<Integer>) HSConfig.getList("Application", "FullScreen", "EnableVersionList");
+        int versionCode = HSApplication.getFirstLaunchInfo().appVersionCode;
+        if (enableList != null && enableList.contains(versionCode)) {
+            return true;
+        }
+        return false;
     }
 }
