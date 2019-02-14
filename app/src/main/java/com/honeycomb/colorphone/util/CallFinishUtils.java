9eb61db0739ba6368cab78ff2e850e7e2b995de8
duanxing.sun <duanxing.sun@ihandysoft.com>
2019-02-14 14:56:34 +0800

    Add config "Application", "CallFinishWire", "DisableAfterInstallDays"

--- app/src/main/java/com/honeycomb/colorphone/util/CallFinishUtils.java
+++ app/src/main/java/com/honeycomb/colorphone/util/CallFinishUtils.java
@@ -1,5 +1,8 @@
 package com.honeycomb.colorphone.util;
 
+import android.text.format.DateUtils;
+
+import com.colorphone.lock.util.ConfigUtils;
 import com.ihs.app.framework.HSApplication;
 import com.ihs.commons.config.HSConfig;
 import com.ihs.commons.utils.HSLog;
@@ -13,11 +16,19 @@ public class CallFinishUtils {
     public static boolean isCallFinishFullScreenAdEnabled() {
         boolean configEnable = HSConfig.optBoolean(false, "Application", "CallFinishWire", "Enable")
                 || enabledThisVersion();
+        boolean limitUser = userLimitByInstallTimes();
 
         HSLog.d("CallFinish", "config enable : " + configEnable );
-        return configEnable;
+        return configEnable && !limitUser;
     }
 
+    private static boolean userLimitByInstallTimes() {
+        int disableDays = HSConfig.optInteger(-1, "Application", "CallFinishWire", "DisableAfterInstallDays");
+        if (disableDays > 0) {
+            return System.currentTimeMillis() - ConfigUtils.getAppFirstInstallTime() > disableDays * DateUtils.DAY_IN_MILLIS;
+        }
+        return false;
+    }
 
     private static boolean enabledThisVersion() {
         List<Integer> enableList = (List<Integer>) HSConfig.getList("Application", "CallFinishWire", "EnableVersionList");
