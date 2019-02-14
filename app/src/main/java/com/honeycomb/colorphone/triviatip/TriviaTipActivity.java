5694cc295cf55078afe852d959b9e607acfd12d3
duanxing.sun <duanxing.sun@ihandysoft.com>
2019-02-14 19:16:36 +0800

    TriviaActivity fullscreen.

--- app/src/main/java/com/honeycomb/colorphone/triviatip/TriviaTipActivity.java
+++ app/src/main/java/com/honeycomb/colorphone/triviatip/TriviaTipActivity.java
@@ -1,15 +1,20 @@
 package com.honeycomb.colorphone.triviatip;
 
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.honeycomb.colorphone.R;
 import com.ihs.commons.utils.HSLog;
 
 import net.appcloudbox.autopilot.AutopilotEvent;
+
+import static android.view.WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN;
 
 public class TriviaTipActivity extends AppCompatActivity implements TriviaTipLayout.onTipDismissListener {
     private static final String TAG = TriviaTipActivity.class.getSimpleName();
@@ -19,6 +24,17 @@ public class TriviaTipActivity extends AppCompatActivity implements TriviaTipLay
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Window window = getWindow();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        window.setSoftInputMode(SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
         TriviaItem item = (TriviaItem) getIntent().getSerializableExtra(EXTRA_ITEM);
         showTip(item);
         AutopilotEvent.onExtendedActive();
