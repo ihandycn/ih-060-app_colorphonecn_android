642dc3a8e6fd2a6ddafa622c00357c7814485d1f
duanxing.sun <duanxing.sun@ihandysoft.com>
2019-02-14 15:37:54 +0800

    Fix config : "Application", "FullScreen", "DisableAfterInstallDays"

--- app/src/main/java/com/honeycomb/colorphone/ad/ConfigSettings.java
+++ app/src/main/java/com/honeycomb/colorphone/ad/ConfigSettings.java
@@ -1,5 +1,8 @@
 package com.honeycomb.colorphone.ad;
 
+import android.text.format.DateUtils;
+
+import com.colorphone.lock.util.ConfigUtils;
 import com.honeycomb.colorphone.Ap;
 import com.ihs.app.framework.HSApplication;
 import com.ihs.commons.config.HSConfig;
@@ -12,14 +15,14 @@ public class ConfigSettings {
         boolean configEnable = HSConfig.optBoolean(false,"Application", "FullScreen", "ThemeApply");
         boolean autopilotEnable = Ap.DetailAd.enableAdOnApply();
         HSLog.d("ConfigSettings", "AdOnApplyTheme:" + configEnable + "," + autopilotEnable);
-        return (configEnable || enabledThisVersion()) && autopilotEnable;
+        return (configEnable || enabledThisVersion()) && autopilotEnable && !userLimitByInstallTimes();
     }
 
     public static boolean showAdOnDetailView() {
         boolean configEnable = HSConfig.optBoolean(false,"Application", "FullScreen", "DetailView");
         boolean autopilotEnable = Ap.DetailAd.enableAdOnDetailView();
         HSLog.d("ConfigSettings", "AdOnDetailView:" + configEnable + "," + autopilotEnable);
-        return (configEnable || enabledThisVersion()) && autopilotEnable;
+        return (configEnable || enabledThisVersion()) && autopilotEnable && !userLimitByInstallTimes();
     }
 
     private static boolean enabledThisVersion() {
@@ -27,6 +30,14 @@ public class ConfigSettings {
         int versionCode = HSApplication.getFirstLaunchInfo().appVersionCode;
         if (enableList != null && enableList.contains(versionCode)) {
             return true;
+        }
+        return false;
+    }
+
+    private static boolean userLimitByInstallTimes() {
+        int disableDays = HSConfig.optInteger(-1, "Application", "FullScreen", "DisableAfterInstallDays");
+        if (disableDays > 0) {
+            return System.currentTimeMillis() - ConfigUtils.getAppFirstInstallTime() > disableDays * DateUtils.DAY_IN_MILLIS;
         }
         return false;
     }
--- app/src/main/java/com/honeycomb/colorphone/util/CallFinishUtils.java
+++ app/src/main/java/com/honeycomb/colorphone/util/CallFinishUtils.java
@@ -1,8 +1,5 @@
 package com.honeycomb.colorphone.util;
 
-import android.text.format.DateUtils;
-
-import com.colorphone.lock.util.ConfigUtils;
 import com.ihs.app.framework.HSApplication;
 import com.ihs.commons.config.HSConfig;
 import com.ihs.commons.utils.HSLog;
@@ -16,18 +13,9 @@ public class CallFinishUtils {
     public static boolean isCallFinishFullScreenAdEnabled() {
         boolean configEnable = HSConfig.optBoolean(false, "Application", "CallFinishWire", "Enable")
                 || enabledThisVersion();
-        boolean limitUser = userLimitByInstallTimes();
 
         HSLog.d("CallFinish", "config enable : " + configEnable );
-        return configEnable && !limitUser;
-    }
-
-    private static boolean userLimitByInstallTimes() {
-        int disableDays = HSConfig.optInteger(-1, "Application", "CallFinishWire", "DisableAfterInstallDays");
-        if (disableDays > 0) {
-            return System.currentTimeMillis() - ConfigUtils.getAppFirstInstallTime() > disableDays * DateUtils.DAY_IN_MILLIS;
-        }
-        return false;
+        return configEnable;
     }
 
     private static boolean enabledThisVersion() {
