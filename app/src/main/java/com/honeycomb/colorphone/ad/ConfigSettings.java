1fd9cb013bf63d2d17043498f4e24ad22f815160
duanxing.sun <duanxing.sun@ihandysoft.com>
2019-02-16 17:37:24 +0800

    Remove autopilot config for ThemeWire master-switch.

--- app/src/main/java/com/honeycomb/colorphone/ad/ConfigSettings.java
+++ app/src/main/java/com/honeycomb/colorphone/ad/ConfigSettings.java
@@ -3,7 +3,6 @@ package com.honeycomb.colorphone.ad;
 import android.text.format.DateUtils;
 
 import com.colorphone.lock.util.ConfigUtils;
-import com.honeycomb.colorphone.Ap;
 import com.ihs.app.framework.HSApplication;
 import com.ihs.commons.config.HSConfig;
 import com.ihs.commons.utils.HSLog;
@@ -12,17 +11,17 @@ import java.util.List;
 
 public class ConfigSettings {
     public static boolean showAdOnApplyTheme() {
-        boolean configEnable = HSConfig.optBoolean(false,"Application", "FullScreen", "ThemeApply");
-        boolean autopilotEnable = Ap.DetailAd.enableAdOnApply();
-        HSLog.d("ConfigSettings", "AdOnApplyTheme:" + configEnable + "," + autopilotEnable);
-        return (configEnable || enabledThisVersion()) && autopilotEnable && !userLimitByInstallTimes();
+        boolean configEnable = HSConfig.optBoolean(false,"Application", "FullScreen", "ThemeApply")
+                || enabledThisVersion();
+         HSLog.d("ConfigSettings", "AdOnApplyTheme:" + configEnable);
+        return (configEnable ) && !userLimitByInstallTimes();
     }
 
     public static boolean showAdOnDetailView() {
-        boolean configEnable = HSConfig.optBoolean(false,"Application", "FullScreen", "DetailView");
-        boolean autopilotEnable = Ap.DetailAd.enableAdOnDetailView();
-        HSLog.d("ConfigSettings", "AdOnDetailView:" + configEnable + "," + autopilotEnable);
-        return (configEnable || enabledThisVersion()) && autopilotEnable && !userLimitByInstallTimes();
+        boolean configEnable = HSConfig.optBoolean(false,"Application", "FullScreen", "DetailView")
+                || enabledThisVersion();
+        HSLog.d("ConfigSettings", "AdOnDetailView:" + configEnable );
+        return (configEnable) && !userLimitByInstallTimes();
     }
 
     private static boolean enabledThisVersion() {
