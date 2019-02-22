f62fca6978f494da46ed9e9254963abaa9e143a7
duanxing.sun <duanxing.sun@ihandysoft.com>
2019-02-22 14:39:22 +0800

    Change share media type from "image/*" to "video/mp4".

--- app/src/main/java/com/honeycomb/colorphone/activity/ShareAlertActivity.java
+++ app/src/main/java/com/honeycomb/colorphone/activity/ShareAlertActivity.java
@@ -29,6 +29,7 @@ import com.acb.call.customize.ScreenFlashManager;
 import com.acb.call.customize.ScreenFlashSettings;
 import com.acb.call.themes.LEDAnimationView;
 import com.acb.call.themes.Type;
+import com.acb.call.utils.FileUtils;
 import com.acb.call.views.CircleImageView;
 import com.acb.call.views.InCallActionView;
 import com.acb.call.views.ThemePreviewWindow;
@@ -39,7 +40,6 @@ import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
 import com.honeycomb.colorphone.R;
 import com.honeycomb.colorphone.receiver.ShareReceiver;
-import com.honeycomb.colorphone.util.Analytics;
 import com.honeycomb.colorphone.util.ColorPhoneCrashlytics;
 import com.honeycomb.colorphone.util.ShareAlertAutoPilotUtils;
 import com.honeycomb.colorphone.util.Utils;
@@ -47,6 +47,7 @@ import com.honeycomb.colorphone.view.FixRatioPreviewWindow;
 import com.honeycomb.colorphone.view.GlideApp;
 import com.ihs.commons.utils.HSLog;
 import com.superapps.util.Preferences;
+import com.superapps.util.Threads;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -54,6 +55,8 @@ import java.io.IOException;
 import java.io.Serializable;
 
 public class ShareAlertActivity extends Activity {
+
+    private static final String TAG = ShareAlertActivity.class.getSimpleName();
 
     public static class UserInfo implements Serializable {
         private String phoneNumber;
@@ -194,9 +197,16 @@ public class ShareAlertActivity extends Activity {
 
         switch (requestCode) {
             case REQUEST_SHARE:
+                HSLog.d(TAG, "Request share over");
                 Utils.deleteRecursive(getTempShareDirectory(this));
                 break;
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        Utils.deleteRecursive(getTempShareDirectory(this));
+        super.onDestroy();
     }
 
     private void initThemePreviewWindow() {
@@ -313,6 +323,34 @@ public class ShareAlertActivity extends Activity {
         });
     }
 
+    private boolean shareVideo(Type themeType) {
+        File videoFile = new File(FileUtils.getMediaDirectory(), themeType.getFileName());
+
+        File shareFile = null;
+        if (android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED)) {
+            File shareDir = getTempShareDirectory(this);
+            shareFile = new File(shareDir.getAbsolutePath(),  themeType.getName() + ".mp4");
+        }
+        if (videoFile.exists() && shareFile != null) {
+            File finalShareFile = shareFile;
+            Threads.postOnThreadPoolExecutor(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Utils.copyFile(videoFile, finalShareFile);
+                        doShareFile(finalShareFile, "video/mp4");
+                        HSLog.d(TAG, "Share video success");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        HSLog.e(TAG, "Share video fail!");
+                    }
+                }
+            });
+            return true;
+        }
+        return false;
+    }
+
     private void share(View cardView) {
         editUserView(cardView);
         Bitmap bitmap = getScreenViewBitmap(cardView);
@@ -325,10 +363,6 @@ public class ShareAlertActivity extends Activity {
         try {
             if (android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED)) {
                 File shareDir = getTempShareDirectory(this);
-                if (!shareDir.exists()) {
-                    shareDir.mkdirs();
-                }
-
                 String filepath = shareDir.getAbsolutePath() + "/" + themeType.getName() + ".jpg";
                 File tempFile = new File(filepath);
                 FileOutputStream ostream = new FileOutputStream(tempFile);
@@ -338,26 +372,7 @@ public class ShareAlertActivity extends Activity {
                 ostream.close();
 
                 File shareFile = new File(filepath);
-                Intent share = new Intent(Intent.ACTION_SEND);
-                share.setType("image/*");
-                share.putExtra(Intent.EXTRA_TEXT,
-                        isInsideApp ? ShareAlertAutoPilotUtils.getInsideAppShareText()
-                                : ShareAlertAutoPilotUtils.getOutsideAppShareText());
-                share.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(shareFile));
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
-                    Intent receiver = new Intent(this, ShareReceiver.class);
-                    receiver.putExtra(IS_INSIDE_APP, isInsideApp);
-                    receiver.putExtra(ShareReceiver.THEME_NAME, themeType.getName());
-                    receiver.putExtra(ShareReceiver.IS_SET_FOR_SOMEONE, isSetForSomeone);
-                    receiver.putExtra(ShareReceiver.IS_CONTACT, isContactInApp);
-                    PendingIntent pendingIntent = PendingIntent.getBroadcast(this, REQUEST_SHARE, receiver, PendingIntent.FLAG_UPDATE_CURRENT);
-                    Intent chooser = Intent.createChooser(share, getResources().getString(R.string.app_name), pendingIntent.getIntentSender());
-                    startActivityForResult(chooser, REQUEST_SHARE);
-                } else {
-                    Intent chooser = Intent.createChooser(share, getResources().getString(R.string.app_name));
-                    startActivityForResult(chooser, REQUEST_SHARE);
-                }
+                doShareFile(shareFile, "image/*");
             }
         } catch (IOException | ActivityNotFoundException e) {
             e.printStackTrace();
@@ -365,11 +380,39 @@ public class ShareAlertActivity extends Activity {
         finish();
     }
 
+    private void doShareFile(File shareFile, String intentFormatType) {
+        Intent share = new Intent(Intent.ACTION_SEND);
+        share.setType(intentFormatType);
+        share.putExtra(Intent.EXTRA_TEXT,
+                isInsideApp ? ShareAlertAutoPilotUtils.getInsideAppShareText()
+                        : ShareAlertAutoPilotUtils.getOutsideAppShareText());
+        share.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(shareFile));
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+            Intent receiver = new Intent(this, ShareReceiver.class);
+            receiver.putExtra(IS_INSIDE_APP, isInsideApp);
+            receiver.putExtra(ShareReceiver.THEME_NAME, themeType.getName());
+            receiver.putExtra(ShareReceiver.IS_SET_FOR_SOMEONE, isSetForSomeone);
+            receiver.putExtra(ShareReceiver.IS_CONTACT, isContactInApp);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(this, REQUEST_SHARE, receiver, PendingIntent.FLAG_UPDATE_CURRENT);
+            Intent chooser = Intent.createChooser(share, getResources().getString(R.string.app_name), pendingIntent.getIntentSender());
+            startActivityForResult(chooser, REQUEST_SHARE);
+        } else {
+            Intent chooser = Intent.createChooser(share, getResources().getString(R.string.app_name));
+            startActivityForResult(chooser, REQUEST_SHARE);
+        }
+    }
+
     private void setImageCoverAndShare(final View root) {
         if (themeType.getValue() == Type.TECH || themeType.getValue() == Type.LED) {
             share(root);
             return;
         }
+
+        if (themeType.isVideo() && shareVideo(themeType)) {
+            return;
+        }
+
         final ImageView previewImage = root.findViewById(R.id.preview_image);
         getBitmap(themeType.getPreviewImage(), Utils.getPhoneWidth(this), Utils.getPhoneHeight(this), new BitmapFetcher() {
             @Override
@@ -464,7 +507,10 @@ public class ShareAlertActivity extends Activity {
     }
 
     private File getTempShareDirectory(Context context) {
-        return new File(context.getExternalFilesDir(null), "ColorPhone");
+        File file = new File(context.getExternalFilesDir(null), "Share");
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        return file;
     }
-
 }
