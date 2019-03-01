97ac9ac2415a735755138699995c1002a851562f
zhewang <zhe.wang@ihandysoft.com>
2019-03-01 20:48:09 +0800

    use different strings for roms

--- permissions/src/main/java/com/acb/colorphone/permissions/NotificationOnLockScreenGuideActivity.java
+++ permissions/src/main/java/com/acb/colorphone/permissions/NotificationOnLockScreenGuideActivity.java
@@ -1,9 +1,23 @@
 package com.acb.colorphone.permissions;
 
+import com.superapps.util.rom.RomUtils;
+
 public class NotificationOnLockScreenGuideActivity extends SimplePermissionGuideActivity {
 
     @Override
     protected int getTitleStringResId() {
-        return R.string.acb_phone_grant_notification_onlockscreen_access_title;
+        if (RomUtils.checkIsHuaweiRom()) {
+            return R.string.acb_phone_grant_show_onlockscreen_access_title_huawei;
+        }
+        if (RomUtils.checkIsOppoRom()) {
+            return R.string.acb_phone_grant_show_onlockscreen_access_title_oppo;
+        }
+        if (RomUtils.checkIsVivoRom()) {
+            return R.string.acb_phone_grant_show_onlockscreen_access_title_vivo;
+        }
+        if (RomUtils.checkIsMiuiRom()) {
+            return R.string.acb_phone_grant_show_onlockscreen_access_title_miui;
+        }
+        return R.string.acb_phone_grant_show_onlockscreen_access_title;
     }
 }
