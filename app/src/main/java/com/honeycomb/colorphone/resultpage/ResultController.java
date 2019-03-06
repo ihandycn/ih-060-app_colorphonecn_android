1b46e4789fc5867e4f03d71e7a34adb5268ed140
zhewang <zhe.wang@ihandysoft.com>
2019-03-06 17:42:08 +0800

    remove flurry event

--- app/src/main/java/com/honeycomb/colorphone/resultpage/ResultController.java
+++ app/src/main/java/com/honeycomb/colorphone/resultpage/ResultController.java
@@ -486,7 +486,6 @@ abstract class ResultController implements View.OnClickListener {
                 LauncherAnalytics.logEvent("Colorphone_CPUDone_Page_Shown");
                 break;
             case ResultConstants.RESULT_TYPE_THEME_RECOMMEND:
-                ThemeRecommendManager.logThemeRecommendDoneShow();
                 break;
         }
     }
