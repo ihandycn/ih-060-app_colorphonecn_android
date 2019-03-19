d72d92162fc2e615ff5322a4131fe682e4d04654
zhewang <zhe.wang@ihandysoft.com>
2019-03-19 13:55:29 +0800

    add strings for roms

--- permissions/src/main/java/com/acb/colorphone/permissions/NotificationMIUIGuideActivity.java
+++ permissions/src/main/java/com/acb/colorphone/permissions/NotificationMIUIGuideActivity.java
@@ -1,9 +1,23 @@
 package com.acb.colorphone.permissions;
 
+import com.superapps.util.rom.RomUtils;
+
 public class NotificationMIUIGuideActivity extends LottiePermissionGuideActivity {
 
     @Override
     protected int getTitleStringResId() {
+        if (RomUtils.checkIsHuaweiRom()) {
+            return R.string.acb_phone_grant_notification_access_title_huawei;
+        }
+        if (RomUtils.checkIsOppoRom()) {
+            return R.string.acb_phone_grant_notification_access_title_oppo;
+        }
+        if (RomUtils.checkIsVivoRom()) {
+            return R.string.acb_phone_grant_notification_access_title_vivo;
+        }
+        if (RomUtils.checkIsMiuiRom()) {
+            return R.string.acb_phone_grant_notification_access_title_miui;
+        }
         return R.string.acb_phone_grant_notification_access_title;
     }
 
