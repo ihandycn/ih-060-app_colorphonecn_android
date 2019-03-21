304f8654428297b1d1bd43949b9e0c0d235d2353
duanxing.sun <duanxing.sun@ihandysoft.com>
2019-03-21 11:30:28 +0800

    Fix build

--- app/src/main/java/com/honeycomb/colorphone/receiver/PackageAddReceiver.java
+++ app/src/main/java/com/honeycomb/colorphone/receiver/PackageAddReceiver.java
@@ -5,13 +5,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.text.TextUtils;
 
-import com.honeycomb.colorphone.activity.PromoteLockerActivity;
 import com.honeycomb.colorphone.boost.SystemAppsManager;
 import com.honeycomb.colorphone.util.AvatarAutoPilotUtils;
 import com.honeycomb.colorphone.util.LauncherAnalytics;
-import com.honeycomb.colorphone.util.PromoteLockerAutoPilotUtils;
 import com.ihs.commons.utils.HSLog;
-import com.superapps.util.Preferences;
 import com.superapps.util.Threads;
 
 
@@ -29,26 +26,14 @@ public class PackageAddReceiver extends BroadcastReceiver {
                 || Intent.ACTION_PACKAGE_INSTALL.equals(intent.getAction())) {
             final String pkgAdd = intent.getData().getSchemeSpecificPart();
             HSLog.d("PackageAddReceiver", "Pkg add :" + pkgAdd);
-            String pkgName = PromoteLockerAutoPilotUtils.getPromoteLockerApp();
             Threads.postOnMainThread(new Runnable() {
                 @Override
                 public void run() {
                     SystemAppsManager.getInstance().addPackage(pkgAdd);
                 }
             });
-            HSLog.d("PackageAddReceiver", "Pkg local read :" + pkgName);
 
-            if (TextUtils.equals(pkgName, pkgAdd)) {
-                int promoteLockerAlertType = Preferences.get(PromoteLockerActivity.PREFS_FILE).getInt(PromoteLockerActivity.ALERT_TYPE, INSTALL_NOT_BY_PROMOTE);
-                if (promoteLockerAlertType == PromoteLockerActivity.WHEN_APP_LAUNCH) {
-                    LauncherAnalytics.logEvent("StartApp_Promote_App_Downloaded");
-                } else if (promoteLockerAlertType == PromoteLockerActivity.AFTER_APPLY_FINISH){
-                    LauncherAnalytics.logEvent("ApplyFinished_Promote_App_Downloaded");
-                }
-                if (promoteLockerAlertType != INSTALL_NOT_BY_PROMOTE) {
-                    PromoteLockerAutoPilotUtils.logPromoteLockerDownloaded();
-                }
-            } else if (TextUtils.equals(AvatarAutoPilotUtils.CAMERA_PKG_NAME, pkgAdd)) {
+           if (TextUtils.equals(AvatarAutoPilotUtils.CAMERA_PKG_NAME, pkgAdd)) {
                 LauncherAnalytics.logEvent("Colorphone_AvatarApp_Download", "AvatarType", AvatarAutoPilotUtils.CAMERA_NAME);
             } else if (TextUtils.equals(AvatarAutoPilotUtils.HEAD_PKG_NAME, pkgAdd)) {
                 LauncherAnalytics.logEvent("Colorphone_AvatarApp_Download", "AvatarType", AvatarAutoPilotUtils.HEAD_NAME);
