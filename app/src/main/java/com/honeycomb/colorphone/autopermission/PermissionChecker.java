7cd46cbca1628f93d0ede7c093687218a4a5dad9
duanxing.sun <duanxing.sun@ihandysoft.com>
2019-04-02 15:01:36 +0800

    Add PermissionChecker for permissions that customized by Domestic-Manufactor.

---
+++ app/src/main/java/com/honeycomb/colorphone/autopermission/PermissionChecker.java
@@ -0,0 +1,48 @@
+package com.honeycomb.colorphone.autopermission;
+
+import com.honeycomb.colorphone.Constants;
+import com.ihs.app.framework.HSApplication;
+import com.superapps.util.Compats;
+import com.superapps.util.Permissions;
+import com.superapps.util.Preferences;
+
+/**
+ * @author sundxing
+ */
+public class PermissionChecker {
+
+    public static boolean hasFloatWindowPermission() {
+        boolean systemResult = Permissions.isFloatWindowAllowed(HSApplication.getContext());
+        if (!Compats.IS_XIAOMI_DEVICE || !Compats.IS_HUAWEI_DEVICE) {
+            return systemResult;
+        }
+        return Preferences.get(Constants.PREF_FILE_DEFAULT).getBoolean("prefs_float_permission",
+                systemResult);
+    }
+
+    public static void onFloatPermissionChange(boolean hasPermission) {
+        Preferences.get(Constants.PREF_FILE_DEFAULT).putBoolean("prefs_float_permission", hasPermission);
+    }
+
+    public static void onAutoStartChange(boolean hasPermission) {
+        Preferences.get(Constants.PREF_FILE_DEFAULT).putBoolean("prefs_auto_start_permission", hasPermission);
+    }
+
+    public static boolean hasAutoStartPermission() {
+        return Preferences.get(Constants.PREF_FILE_DEFAULT).getBoolean("prefs_auto_start_permission", false);
+    }
+
+    public static void onShowOnLockScreenChange(boolean hasPermission) {
+        Preferences.get(Constants.PREF_FILE_DEFAULT).putBoolean("prefs_show_on_lockscreen_permission", hasPermission);
+    }
+
+    public static boolean hasShowOnLockScreenPermission() {
+        if (Compats.IS_XIAOMI_DEVICE) {
+            return Preferences.get(Constants.PREF_FILE_DEFAULT).getBoolean("prefs_show_on_lockscreen_permission",
+                    false);
+        } else {
+            // TODO
+            return true;
+        }
+    }
+}
