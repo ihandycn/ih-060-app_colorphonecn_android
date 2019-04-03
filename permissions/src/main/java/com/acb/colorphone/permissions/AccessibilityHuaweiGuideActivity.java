eb27e376a4a1b026eb319dcbb11877fa610e2c8b
zhewang <zhe.wang@ihandysoft.com>
2019-04-03 20:57:44 +0800

    fix huawei guide issue

--- permissions/src/main/java/com/acb/colorphone/permissions/AccessibilityHuaweiGuideActivity.java
+++ permissions/src/main/java/com/acb/colorphone/permissions/AccessibilityHuaweiGuideActivity.java
@@ -12,6 +12,6 @@ public class AccessibilityHuaweiGuideActivity extends LottiePermissionGuideActiv
     }
 
     @Override protected String getAnimationFromJson() {
-        return "lottie/acb_phone_permission_accessibility_huawei";
+        return "lottie/acb_phone_permission_accessibility_huawei.json";
     }
 }
