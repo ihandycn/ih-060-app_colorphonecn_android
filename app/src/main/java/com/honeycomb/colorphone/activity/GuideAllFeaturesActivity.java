3962714e8b34e8bae017444a5b9e7aa147e746db
duanxing.sun <duanxing.sun@ihandysoft.com>
2019-06-12 19:53:12 +0800

    Not request ANSWER_PHONE_CALLS on first start.

--- app/src/main/java/com/honeycomb/colorphone/activity/GuideAllFeaturesActivity.java
+++ app/src/main/java/com/honeycomb/colorphone/activity/GuideAllFeaturesActivity.java
@@ -135,9 +135,6 @@ public class GuideAllFeaturesActivity extends HSAppCompatActivity {
 
     private void buildPermissionList() {
         perms.add(Manifest.permission.READ_PHONE_STATE);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            perms.add(Manifest.permission.ANSWER_PHONE_CALLS);
-        }
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             perms.add(Manifest.permission.READ_CALL_LOG);
         }
