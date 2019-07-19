7442e710a50a484760dc04bab9da1fbed34c78b6
duanxing.sun <duanxing.sun@ihandysoft.com>
2019-07-19 20:53:29 +0800

    Dismiss non-secure keyguard on screen on/off. Fix issue #24

--- lock/src/main/java/com/colorphone/lock/lockscreen/BaseKeyguardActivity.java
+++ lock/src/main/java/com/colorphone/lock/lockscreen/BaseKeyguardActivity.java
@@ -11,10 +11,13 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager.LayoutParams;
 
+import com.colorphone.lock.LockerCustomConfig;
 import com.colorphone.lock.lockscreen.chargingscreen.ChargingScreenUtils;
 import com.ihs.app.alerts.HSAlertMgr;
 import com.ihs.app.framework.activity.HSAppCompatActivity;
 import com.ihs.commons.utils.HSLog;
+import com.ihs.device.common.utils.Utils;
+import com.superapps.util.Threads;
 
 import static android.view.WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD;
 import static android.view.WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN;
@@ -23,11 +26,33 @@ public abstract class BaseKeyguardActivity extends HSAppCompatActivity {
 
     public static boolean exist;
 
+    private KeyguardManager keyguardManager;
+    private boolean isKeyguardSecure;
+
+    /**
+     * We should ignore event of UserPresent that trigger by ourself.
+     * but, if user trigger it , we should finish out ourself.
+     * (In case face detect success, user back to home screen in time)
+     */
+    private boolean ingoreUserPresentEvent;
+
     BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            HSLog.d("Locker", "UserPresent");
-            finish();
+            HSLog.d("Locker", "UserPresent, ingore = " + ingoreUserPresentEvent);
+            if (ingoreUserPresentEvent) {
+                ingoreUserPresentEvent = false;
+            } else {
+                finish();
+            }
+        }
+    };
+
+    private Runnable mUserPresentTimeoutChecker = new Runnable() {
+        @Override
+        public void run() {
+            // Set flag to false.
+            ingoreUserPresentEvent = false;
         }
     };
 
@@ -63,20 +88,23 @@ public abstract class BaseKeyguardActivity extends HSAppCompatActivity {
         window.setSoftInputMode(SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
         // Check keyguard
-        boolean keyguardFlag = false;
-        KeyguardManager keyguardManager = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
+        isKeyguardSecure = false;
+        keyguardManager = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
         if (keyguardManager != null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-            keyguardFlag = keyguardManager.isKeyguardSecure();
-            HSLog.d("isKeyguardSecure: " + keyguardManager.isKeyguardSecure()
+            isKeyguardSecure = keyguardManager.isKeyguardSecure();
+            HSLog.d("isKeyguardSecure: " + isKeyguardSecure
                     + " isKeyguardLocked: " + keyguardManager.isKeyguardLocked());
         }
-        if (!keyguardFlag) {
-            if (keyguardManager != null &&
-                    Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                keyguardManager.requestDismissKeyguard(this, null);
-            } else {
-                getWindow().addFlags(FLAG_DISMISS_KEYGUARD);
-            }
+
+        if (!isKeyguardSecure) {
+            tryDismissKeyguard();
+        }
+
+        boolean isScreenOn = Utils.isScreenOn();
+        if (!isScreenOn) {
+            // Log only screen off,
+            // When Screen On user may unlock screen by FaceDetect or FingerPrint
+            LockerCustomConfig.getLogger().logEvent("LockScreen_Keyguard_User", "Type", isKeyguardSecure ? "Secure" : "None");
         }
 
         IntentFilter intentFilter = new IntentFilter(Intent.ACTION_USER_PRESENT);
@@ -85,10 +113,33 @@ public abstract class BaseKeyguardActivity extends HSAppCompatActivity {
         onInitView();
     }
 
+    private void tryDismissKeyguard() {
+        if (keyguardManager != null &&
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            keyguardManager.requestDismissKeyguard(this, null);
+        } else {
+            getWindow().addFlags(FLAG_DISMISS_KEYGUARD);
+        }
+
+        // Trigger by ourself.
+        ingoreUserPresentEvent = true;
+        Threads.removeOnMainThread(mUserPresentTimeoutChecker);
+        Threads.postOnMainThread(mUserPresentTimeoutChecker);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (!isKeyguardSecure) {
+           tryDismissKeyguard();
+        }
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
         unregisterReceiver(mBroadcastReceiver);
+        Threads.removeOnMainThread(mUserPresentTimeoutChecker);
     }
 
     protected abstract void onInitView();
