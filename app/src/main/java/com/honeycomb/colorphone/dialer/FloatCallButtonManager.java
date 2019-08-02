75952302d576728c0768f672af798d754d770984
yu.zhang <yu.zhang@ihandysoft.com>
2019-08-02 19:33:03 +0800

    Add Lottie animation for floating button of dialer

--- app/src/main/java/com/honeycomb/colorphone/dialer/FloatCallButtonManager.java
+++ app/src/main/java/com/honeycomb/colorphone/dialer/FloatCallButtonManager.java
@@ -6,7 +6,10 @@ import android.os.SystemClock;
 import android.support.annotation.RequiresApi;
 import android.view.View;
 import android.widget.Chronometer;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.honeycomb.colorphone.dialer.call.CallList;
 import com.honeycomb.colorphone.dialer.call.DialerCall;
 import com.honeycomb.colorphone.dialer.util.CallFloatButton;
@@ -17,6 +20,9 @@ public class FloatCallButtonManager implements
 
 
     private Chronometer bottomTimerView;
+    private LottieAnimationView mInCallDesktopButton;
+    private LinearLayout mCallAnsweringLayout;
+    private ImageView mCallAnsweringView;
 
     private DialerCall mOutCall;
     private boolean isTimerStarted;
@@ -31,7 +37,10 @@ public class FloatCallButtonManager implements
                 return;
             }
             mCallFloatButton = new CallFloatButton(context);
+            mCallAnsweringLayout = mCallFloatButton.getmCallAnsweringLayout();
+            mCallAnsweringView = mCallFloatButton.getmCallAnsweringView();
             bottomTimerView = mCallFloatButton.getCallDurationView();
+            mInCallDesktopButton = mCallFloatButton.getInCallDestopButton();
             mCallFloatButton.show();
             isTimerStarted = false;
             updateContactIfChanged();
@@ -43,6 +52,9 @@ public class FloatCallButtonManager implements
         if (mCallFloatButton != null) {
             mCallFloatButton.dismiss();
             bottomTimerView = null;
+            mCallAnsweringLayout = null;
+            mCallAnsweringView = null;
+            mInCallDesktopButton = null;
             mCallFloatButton = null;
             isTimerStarted = false;
         }
@@ -79,7 +91,11 @@ public class FloatCallButtonManager implements
                     && firstCall.getState() == DialerCallState.ACTIVE;
 
             if (isTimerVisible) {
+                mInCallDesktopButton.setAnimation("lottie/call_float_button/dialer_after_answering_lottie.json");
+                mInCallDesktopButton.playAnimation();
+                mCallAnsweringLayout.setVisibility(View.VISIBLE);
                 bottomTimerView.setVisibility(View.VISIBLE);
+                mCallAnsweringView.setVisibility(View.VISIBLE);
                 bottomTimerView.setBase(
                         firstCall.getConnectTimeMillis()
                                 - System.currentTimeMillis()
@@ -93,7 +109,11 @@ public class FloatCallButtonManager implements
                     isTimerStarted = true;
                 }
             } else {
+                mInCallDesktopButton.setAnimation("lottie/call_float_button/dialer_prompt_answer.json");
+                mInCallDesktopButton.playAnimation();
                 bottomTimerView.setVisibility(View.GONE);
+                mCallAnsweringLayout.setVisibility(View.GONE);
+                mCallAnsweringView.setVisibility(View.GONE);
                 bottomTimerView.stop();
                 isTimerStarted = false;
             }
