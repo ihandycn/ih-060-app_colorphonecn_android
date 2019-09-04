ab155c0f31b30bf6ef250207fc45932bc741da99
zhewang <zhe.wang@ihandysoft.com>
2019-09-04 14:18:14 +0800

    add toast for notification access

--- app/src/main/java/com/honeycomb/colorphone/ColorPhoneApplicationImpl.java
+++ app/src/main/java/com/honeycomb/colorphone/ColorPhoneApplicationImpl.java
@@ -25,6 +25,7 @@ import com.acb.call.constant.ScreenFlashConst;
 import com.acb.call.customize.ScreenFlashFactory;
 import com.acb.call.customize.ScreenFlashManager;
 import com.acb.call.utils.FileUtils;
+import com.acb.colorphone.permissions.PermissionConstants;
 import com.call.assistant.customize.CallAssistantConsts;
 import com.call.assistant.customize.CallAssistantManager;
 import com.call.assistant.customize.CallAssistantSettings;
@@ -46,6 +47,7 @@ import com.honeycomb.colorphone.activity.ColorPhoneActivity;
 import com.honeycomb.colorphone.ad.AdManager;
 import com.honeycomb.colorphone.ad.ConfigSettings;
 import com.honeycomb.colorphone.autopermission.AutoLogger;
+import com.honeycomb.colorphone.autopermission.StableToast;
 import com.honeycomb.colorphone.boost.BoostActivity;
 import com.honeycomb.colorphone.boost.DeviceManager;
 import com.honeycomb.colorphone.boost.FloatWindowDialog;
@@ -117,6 +119,7 @@ import com.superapps.util.Dimensions;
 import com.superapps.util.HomeKeyWatcher;
 import com.superapps.util.Preferences;
 import com.superapps.util.Threads;
+import com.superapps.util.rom.RomUtils;
 import com.tencent.bugly.beta.Beta;
 import com.tencent.bugly.crashreport.CrashReport;
 import com.umeng.analytics.MobclickAgent;
@@ -237,6 +240,13 @@ public class ColorPhoneApplicationImpl {
 
                 } else {
                     HSLog.i("CCTest", "not time");
+                }
+            } else if (TextUtils.equals(PermissionConstants.PERMISSION_GUIDE_EXIT, notificationName)) {
+                String permission = bundle.getString(PermissionConstants.PERMISSION_GUIDE_EXIT);
+                if (TextUtils.equals(permission, PermissionConstants.PERMISSION_NOTIFICATION_ACCESS)) {
+                    if (RomUtils.checkIsHuaweiRom()) {
+                        StableToast.showHuaweiNotificationAccessToast();
+                    }
                 }
             }
         }
@@ -815,6 +825,7 @@ public class ColorPhoneApplicationImpl {
         HSGlobalNotificationCenter.addObserver(SlidingDrawerContent.EVENT_SHOW_BLACK_HOLE, mObserver);
         HSGlobalNotificationCenter.addObserver(Constants.NOTIFY_KEY_APP_FULLY_DISPLAY, mObserver);
         HSGlobalNotificationCenter.addObserver(FloatWindowController.NOTIFY_KEY_LOCKER_DISMISS, mObserver);
+        HSGlobalNotificationCenter.addObserver(PermissionConstants.PERMISSION_GUIDE_EXIT, mObserver);
 
         final IntentFilter screenFilter = new IntentFilter();
         screenFilter.addAction(Intent.ACTION_SCREEN_OFF);
--- permissions/src/main/java/com/acb/colorphone/permissions/Constants.java
+++
@@ -1,5 +0,0 @@
-package com.acb.colorphone.permissions;
-
-public class Constants {
-    public static String INTENT_EXTRA_KEY_PERMISSION = "intent_extra_key_permission";
-}
