31562962472c70861ba9336c12e9f11246caca7c
duanxing.sun <duanxing.sun@ihandysoft.com>
2019-09-09 20:09:09 +0800

    Fix : plist file not found in shell script.

--- plist-conv.sh
+++ plist-conv.sh
@@ -1,7 +1,7 @@
 chmod +x ../jacoder
 
 echo "Converting files below to plist archive:$1"
-for file in `find ./build/intermediates/assets/"$1" -type f -name "*.plist"`
+for file in `find ./build/intermediates/ -type f -name "*.plist"`
 do
     echo $file
     ../jacoder -e $file ${file/.plist/.sp}
