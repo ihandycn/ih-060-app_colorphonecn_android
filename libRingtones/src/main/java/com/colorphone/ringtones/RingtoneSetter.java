70d92d97dfbfaea3c848e5e041eb7821d1f2e1a2
zhewang <zhe.wang@ihandysoft.com>
2019-09-23 17:22:05 +0800

    fix libs

--- libRingtones/src/main/java/com/colorphone/ringtones/RingtoneSetter.java
+++
@@ -1,9 +0,0 @@
-package com.colorphone.ringtones;
-
-import com.colorphone.ringtones.module.Ringtone;
-
-public interface RingtoneSetter {
-    boolean onSetRingtone(Ringtone ringtone);
-    boolean onSetAsDefault(Ringtone ringtone);
-    boolean onSetForSomeOne(Ringtone ringtone);
-}
