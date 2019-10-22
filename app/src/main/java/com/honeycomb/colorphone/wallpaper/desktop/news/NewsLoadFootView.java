a6ef3d8be397cb70fb42b12613e23a76ee8f2eee
Haitao Liu <haitao.liu@ihandysoft.com>
2019-10-22 09:54:15 +0800

    Fix class not found crash .

---
+++ app/src/main/java/com/honeycomb/colorphone/wallpaper/desktop/news/NewsLoadFootView.java
@@ -0,0 +1,55 @@
+package com.honeycomb.colorphone.wallpaper.desktop.news;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
+
+import com.honeycomb.colorphone.R;
+import com.honeycomb.colorphone.wallpaper.util.ViewUtils;
+import com.superapps.util.Dimensions;
+
+public class NewsLoadFootView extends LoadFootView {
+
+    private View mProgressBar;
+    private TextView mLoadHint;
+    private TextView mRetryHint;
+
+    private int mHeight = Dimensions.pxFromDp(60);
+
+    public NewsLoadFootView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        if (getVisibility() == View.VISIBLE) {
+            heightMeasureSpec = View.MeasureSpec.makeMeasureSpec(mHeight, View.MeasureSpec.EXACTLY);
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        } else {
+            setMeasuredDimension(0, 0);
+        }
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        mProgressBar = ViewUtils.findViewById(this, R.id.progress_bar);
+        mLoadHint = ViewUtils.findViewById(this, R.id.loading_hint);
+        mRetryHint = ViewUtils.findViewById(this,R.id.retry_hint);
+    }
+
+    @Override
+    public void onLoadFailed() {
+        mProgressBar.setVisibility(GONE);
+        mLoadHint.setVisibility(GONE);
+        mRetryHint.setVisibility(VISIBLE);
+    }
+
+    @Override
+    public void reset() {
+        mProgressBar.setVisibility(VISIBLE);
+        mLoadHint.setVisibility(VISIBLE);
+        mRetryHint.setVisibility(GONE);
+    }
+}
