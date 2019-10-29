bbce073363bb6b4cee86a9f59893b99606799348
Shuai Huang <shuai.huang@ihandysoft.com>
2019-10-29 14:00:23 +0800

    Add toast when login without installed weChat

--- app/src/main/java/com/honeycomb/colorphone/activity/LoginActivity.java
+++ app/src/main/java/com/honeycomb/colorphone/activity/LoginActivity.java
@@ -2,12 +2,15 @@ package com.honeycomb.colorphone.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.honeycomb.colorphone.R;
 import com.honeycomb.colorphone.util.Utils;
@@ -21,10 +24,13 @@ import com.tencent.mm.opensdk.openapi.WXAPIFactory;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+
 public class LoginActivity extends HSAppCompatActivity implements View.OnClickListener {
 
     public static final String APP_ID = "wx5b4bb859043c5e3d";
     private IWXAPI api;
+
     public static void start(Context context) {
         Intent starter = new Intent(context, LoginActivity.class);
         context.startActivity(starter);
@@ -42,9 +48,9 @@ public class LoginActivity extends HSAppCompatActivity implements View.OnClickLi
 
         LinearLayout weixinLoginButton = findViewById(R.id.weixin_login_button);
         weixinLoginButton.setOnClickListener(this);
-        weixinLoginButton.setBackground(BackgroundDrawables.createBackgroundDrawable(0xff19ad3c, Dimensions.pxFromDp(21),true));
+        weixinLoginButton.setBackground(BackgroundDrawables.createBackgroundDrawable(0xff19ad3c, Dimensions.pxFromDp(21), true));
 
-        api = WXAPIFactory.createWXAPI(this,APP_ID,true);
+        api = WXAPIFactory.createWXAPI(this, APP_ID, true);
         api.registerApp(APP_ID);
     }
 
@@ -59,17 +65,43 @@ public class LoginActivity extends HSAppCompatActivity implements View.OnClickLi
 
     @Override
     public void onClick(View view) {
-        switch(view.getId()){
+        switch (view.getId()) {
             case R.id.weixin_login_button:
-
-                SendAuth.Req req = new SendAuth.Req();
-                req.scope = "snsapi_userinfo";
-                req.state = System.currentTimeMillis()+"";
-                api.sendReq(req);
-                Threads.postOnMainThreadDelayed(this::finish,500);
+                if (isWeChatAppInstalled(this)) {
+                    SendAuth.Req req = new SendAuth.Req();
+                    req.scope = "snsapi_userinfo";
+                    req.state = System.currentTimeMillis() + "";
+                    api.sendReq(req);
+                    Threads.postOnMainThreadDelayed(this::finish, 500);
+                } else {
+                    Toast.makeText(this, "您手机暂未安装微信，请重试", Toast.LENGTH_SHORT).show();
+                }
                 break;
             default:
                 break;
+        }
+    }
+
+
+    /**
+     * 判断微信客户端是否存在
+     *
+     * @return true安装, false未安装
+     */
+    public boolean isWeChatAppInstalled(Context context) {
+        api = WXAPIFactory.createWXAPI(context, "Your WeChat AppId");
+        if (api.isWXAppInstalled()) {
+            return true;
+        } else {
+            final PackageManager packageManager = context.getPackageManager();// 获取packagemanager
+            List<PackageInfo> pInfo = packageManager.getInstalledPackages(0);// 获取所有已安装程序的包信息
+            for (int i = 0; i < pInfo.size(); i++) {
+                String pn = pInfo.get(i).packageName;
+                if (pn.equalsIgnoreCase("com.tencent.mm")) {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
