587b9135f73c6e9a1502c21511e0e043a48ff902
yuanwei.bai <yuanwei.bai@infinigames.mobi>
2019-11-27 16:50:42 +0800

    [BuildTT] add event to appsflyer.

--- app/src/main/java/com/honeycomb/colorphone/util/EventUtils.java
+++ app/src/main/java/com/honeycomb/colorphone/util/EventUtils.java
@@ -2,6 +2,7 @@ package com.honeycomb.colorphone.util;
 
 import android.util.Log;
 
+import com.ihs.app.analytics.HSAnalytics;
 import com.ihs.commons.utils.HSPreferenceHelper;
 import com.superapps.util.Calendars;
 import com.superapps.util.Preferences;
@@ -34,6 +35,7 @@ public class EventUtils {
         if (duration > lastDayMills && duration < (lastDayMills + oneDayMills)) {
             Preferences.getDefault().putBoolean(PREF_COLORPHONE_RETENTION_DAY1, true);
             Analytics.logEvent("ColorPhone_Retention_Day1");
+            HSAnalytics.logEventToAppsFlyer("ColorPhone_Retention_Day1");
         }
     }
 }
