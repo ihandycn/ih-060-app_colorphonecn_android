96b43c1778ae2b8dcb5839808c0433ce28a1bebf
yuanwei.bai <yuanwei.bai@infinigames.mobi>
2020-04-21 20:11:58 +0800

    Add acc.

---
+++ app/src/main/java/com/honeycomb/colorphone/debug/CommentReceiver.java
@@ -0,0 +1,29 @@
+package com.honeycomb.colorphone.debug;
+
+import android.graphics.Rect;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.ihs.app.framework.HSApplication;
+import com.ihs.commons.utils.HSLog;
+import com.ihs.permission.acc.AccCommentReceiver;
+
+public class CommentReceiver extends AccCommentReceiver {
+
+    private static final String TAG = "CommentReceiver";
+
+    @Override
+    public void onCommentFirstStep(@NonNull Rect rect) {
+        HSLog.d(TAG, "processName = " + HSApplication.getProcessName() + ", onCommentFirstStep: " + rect.toString());
+    }
+
+    @Override
+    public void onCommentXiaoMiSecondStep() {
+        HSLog.d(TAG, "processName = " + HSApplication.getProcessName() + ", onCommentXiaoMiSecondStep: click");
+    }
+
+    @Override
+    public void onCommentOppoSecondStep(@NonNull Rect rect) {
+        HSLog.d(TAG, "processName = " + HSApplication.getProcessName() + ", onCommentOppoSecondStep: " + rect.toString());
+    }
+}
--- app/src/main/java/com/honeycomb/colorphone/debug/DebugActions.java
+++ app/src/main/java/com/honeycomb/colorphone/debug/DebugActions.java
@@ -2,20 +2,69 @@ package com.honeycomb.colorphone.debug;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.text.TextUtils;
 
-import com.honeycomb.colorphone.http.HttpManager;
-import com.honeycomb.colorphone.util.EventUtils;
+import com.honeycomb.colorphone.feedback.HuaweiRateGuideDialog;
+import com.honeycomb.colorphone.feedback.OppoRateGuideDialog;
+import com.honeycomb.colorphone.feedback.XiaomiRateGuideDialog;
+import com.ihs.app.framework.HSApplication;
+import com.ihs.commons.utils.HSLog;
+import com.ihs.permission.Utils;
+import com.ihs.permission.acc.AccCommentReceiver;
+import com.superapps.util.Compats;
+import com.superapps.util.Threads;
 
 public class DebugActions {
 
     private static final String TAG = "DebugActions";
 
+    private static CommentReceiver commentReceiver;
+
     public static void onVolumeDown(Activity activity) {
-        Intent intent = new Intent(activity, DebugActivity.class);
-        activity.startActivity(intent);
+        boolean isGranted = Utils.isAccessibilityGranted();
+        HSLog.e("rango", " isGranted = " + isGranted);
+
+        activity.unregisterReceiver(commentReceiver);
     }
 
     public static void onVolumeUp(Activity activity) {
-        EventUtils.tryToLogRetentionEvent();
+
+        commentReceiver = new CommentReceiver();
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(AccCommentReceiver.ACTION);
+        activity.registerReceiver(commentReceiver, intentFilter);
+
+        launchAppDetail(HSApplication.getContext().getPackageName(), "com.xiaomi.market");
+    }
+
+    private static void launchAppDetail(String appPkg, String marketPkg) {
+        try {
+            if (TextUtils.isEmpty(appPkg) || TextUtils.isEmpty(marketPkg)) {
+                return;
+            }
+
+            Uri uri = Uri.parse("market://details?id=" + appPkg);
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            if (!TextUtils.isEmpty(marketPkg)) {
+                intent.setPackage(marketPkg);
+            }
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            HSApplication.getContext().startActivity(intent);
+
+            Threads.postOnMainThreadDelayed(() -> {
+                if (Compats.IS_HUAWEI_DEVICE) {
+                    HuaweiRateGuideDialog.show(HSApplication.getContext());
+                } else if (Compats.IS_XIAOMI_DEVICE) {
+                    XiaomiRateGuideDialog.show(HSApplication.getContext());
+                } else if (Compats.IS_OPPO_DEVICE) {
+                    OppoRateGuideDialog.show(HSApplication.getContext());
+                }
+            }, 2000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
