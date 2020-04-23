334c3a0fc912e01a7f29e118f98061e75dc0b6cf
He shuming <shuming.he@infinigames.mobi>
2020-04-23 11:12:50 +0800

    Modify the ad placement

--- app/src/main/java/com/honeycomb/colorphone/Placements.java
+++ app/src/main/java/com/honeycomb/colorphone/Placements.java
@@ -32,7 +32,7 @@ public class Placements {
     public static final String AD_CLEAN_GUIDE = "BoostGuide";  // Express
     public static final String AD_EXIT_WIRE_NEW = "AdExitTextureNew"; // 信息助手退出 Native 拼全屏
 
-    public static final String AD_NEWS_FEED = "NewsFeed";
+    public static final String AD_NEWS_FEED = "NewsFeedAd";
 
     public static final String AD_LOCKER_AND_CHARGING = "CableFeed1";
 
