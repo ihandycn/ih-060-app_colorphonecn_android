0e06a5caedda2a3e438a658abb586c94872d911e
yuanwei.bai <yuanwei.bai@infinigames.mobi>
2020-07-31 11:38:58 +0800

    New locker way.

--- lock/src/main/java/com/colorphone/smartlocker/SmartLockerManager.java
+++ lock/src/main/java/com/colorphone/smartlocker/SmartLockerManager.java
@@ -1,10 +1,19 @@
 package com.colorphone.smartlocker;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.NotificationCompat;
 import android.telephony.TelephonyManager;
+import android.widget.RemoteViews;
 
 import com.colorphone.lock.LockerCustomConfig;
+import com.colorphone.lock.R;
 import com.colorphone.smartlocker.baidu.BaiduFeedManager;
 import com.colorphone.smartlocker.bean.BaiduFeedBean;
 import com.colorphone.smartlocker.bean.BaiduFeedItemsBean;
@@ -88,24 +97,7 @@ public class SmartLockerManager {
             return;
         }
 
-        HSLog.d("Rango", "start SmartLockerFeedsActivity....");
-        Intent intent = new Intent(HSApplication.getContext(), SmartLockerFeedsActivity.class);
-        intent.putExtra(SmartLockerFeedsActivity.EXTRA_INT_BATTERY_LEVEL_PERCENT,
-                HSChargingManager.getInstance().getBatteryRemainingPercent());
-        intent.putExtra(EXTRA_START_TYPE, startType);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP
-                | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NO_ANIMATION);
-
-        try {
-            HSApplication.getContext().startActivity(intent);
-            if (startType == EXTRA_VALUE_START_BY_LOCKER) {
-                LockerCustomConfig.getLogger().logEvent("LockScreen_News_Should_Show", "reason", "Success");
-            } else {
-                LockerCustomConfig.getLogger().logEvent("ChargingScreen_News_Should_Show", "reason", "Success");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        showLockerActivity(startType);
     }
 
     public void tryToPreLoadBaiduNews() {
@@ -142,5 +134,84 @@ public class SmartLockerManager {
 
     public int getShowAdCount() {
         return showNativeCount;
+    }
+
+    private static class NotificationHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == MSG_CANCEL_NOTIFICATION) {
+                NotificationManager notificationManager = (NotificationManager) HSApplication.getContext().getSystemService(Context.NOTIFICATION_SERVICE);
+                notificationManager.cancel("AA_TAG1", 10101);
+            }
+        }
+    }
+
+    private static final int MSG_CANCEL_NOTIFICATION = 100;
+    private NotificationHandler handler = new NotificationHandler();
+
+    private void showLockerActivity(int startType) {
+        Context context = HSApplication.getContext();
+
+        Intent intent = new Intent(HSApplication.getContext(), SmartLockerFeedsActivity.class);
+        intent.putExtra(SmartLockerFeedsActivity.EXTRA_INT_BATTERY_LEVEL_PERCENT,
+                HSChargingManager.getInstance().getBatteryRemainingPercent());
+        intent.putExtra(EXTRA_START_TYPE, startType);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP
+                | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        intent.setAction("inner_action");
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 10102, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        boolean sendSuccess = false;
+        try {
+            pendingIntent.send();
+            if (startType == EXTRA_VALUE_START_BY_LOCKER) {
+                LockerCustomConfig.getLogger().logEvent("LockScreen_News_Should_Show", "reason", "Success");
+            } else {
+                LockerCustomConfig.getLogger().logEvent("ChargingScreen_News_Should_Show", "reason", "Success");
+            }
+            sendSuccess = true;
+        } catch (Exception ignore) {
+        }
+        if (!sendSuccess) {
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            try {
+                context.startActivity(intent);
+                if (startType == EXTRA_VALUE_START_BY_LOCKER) {
+                    LockerCustomConfig.getLogger().logEvent("LockScreen_News_Should_Show", "reason", "Success");
+                } else {
+                    LockerCustomConfig.getLogger().logEvent("ChargingScreen_News_Should_Show", "reason", "Success");
+                }
+            } catch (Exception ignore) {
+            }
+        }
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        setNotificationChannel(context, notificationManager);
+        notificationManager.cancel("AA_TAG1", 10101);
+        notificationManager.notify("AA_TAG1", 10101,
+                new NotificationCompat.Builder(context, "OptimizerApplicationChannel")
+                        .setSmallIcon(R.drawable.charging_screen_guide_close)
+                        .setFullScreenIntent(pendingIntent, true)
+                        .setCustomHeadsUpContentView(new RemoteViews(context.getPackageName(), R.layout.notification_external_content_no_icon_layout))
+                        .build()
+        );
+        handler.removeMessages(MSG_CANCEL_NOTIFICATION);
+        handler.sendEmptyMessageDelayed(MSG_CANCEL_NOTIFICATION, 1000L);
+    }
+
+    public static void setNotificationChannel(Context context, NotificationManager notificationManager) {
+        if (Build.VERSION.SDK_INT >= 26
+                && notificationManager.getNotificationChannel("OptimizerApplicationChannel") == null) {
+            NotificationChannel notificationChannel = new NotificationChannel("OptimizerApplicationChannel",
+                    context.getString(R.string.notification_no_useful_msg), NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.setDescription(context.getString(R.string.notification_no_useful_msg));
+            notificationChannel.setLockscreenVisibility(-1);
+            notificationChannel.enableLights(false);
+            notificationChannel.enableVibration(false);
+            notificationChannel.setShowBadge(false);
+            notificationChannel.setSound(null, null);
+            notificationChannel.setBypassDnd(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
     }
 }
